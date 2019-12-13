81775619154a30447f349896eb11c3b8ab43b12d
Sebastien Maloron <contact@smaloron.eu>
2019-12-13 11:29:42 +0100

    Liste des articles

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/article")
+ * Class ArticleController
+ * @package App\Controller
+ */
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="article-list")
+     */
+    public function index()
+    {
+        $articleList = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        return $this->render('article/index.html.twig', [
+            'articleList' => $articleList,
+        ]);
+    }
+}
